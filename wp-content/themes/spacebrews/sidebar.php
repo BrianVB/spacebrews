2e115458e7c0b4e0c43e461853a0d4a6f7ab09ea
Brian Van Buren <brian@spacebrews.com>
2013-01-15 23:14:03 -0500

    Reformat html layout

--- wp-content/themes/spacebrews/sidebar.php
+++ wp-content/themes/spacebrews/sidebar.php
@@ -6,7 +6,5 @@
 $sidebar_details = sidebar_details();
 ?>
 <div id="<?php echo $sidebar_details['id']; ?>" class="sidebar grid_4">
-	<?php 	/* Widgetized sidebar, if you have the plugin installed. */
-       if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar($sidebar_details['name']) ) : ?>	
-    <?php endif; ?>
+	<?php dynamic_sidebar($sidebar_details['name']); ?>
 </div>
