702085e3652ecdd60d6ec127b726e462c43f0291
Brian Van Buren <brian@spacebrews.com>
2013-01-15 23:13:39 -0500

    Remove textarea size attributes

--- wp-content/themes/spacebrews/comments.php
+++ wp-content/themes/spacebrews/comments.php
@@ -76,9 +76,7 @@
 
 <?php endif; ?>
 
-<!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
-
-<p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
+<p><textarea name="comment" id="comment" tabindex="4"></textarea></p>
 
 <p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
 <?php comment_id_fields(); ?>
