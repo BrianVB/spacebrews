451863ff2f6dd170acebeff2d5b21dfe669fabf4
Brian Van Buren <brian@spacebrews.com>
2013-02-07 21:39:35 -0500

    Add template for a brew

---
+++ wp-content/themes/spacebrews/single-brew.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package AccelerateMedia
+ * @subpackage Accelerate
+ */
+
+get_header();
+?>
+<div id="page">
+    <div id="page-container" class="container_12">
+        <div class="grid_8">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+            <div class="navigation">
+                <div class="left"><?php previous_post_link('&laquo; %link') ?></div>
+                <div class="right"><?php next_post_link('%link &raquo;') ?></div>
+                <div class="clear"></div>
+            </div>
+    
+            <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+                <h2><?php the_title(); ?></h2>
+                <div class="entry">
+                    <?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>    
+                </div>
+            </div>
+    
+        <?php comments_template(); ?>
+    
+        <?php endwhile; else: ?>
+    
+            <p>Sorry, no posts matched your criteria.</p>
+    
+    <?php endif; ?>
+        </div>
+        <?php get_sidebar(); ?>   
+    </div>
+</div>
+<?php get_footer(); ?>
